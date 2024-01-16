bd7d90aae337a15b00ab6d4833ebc8c80a5299b5
TanYongXiangCAP <chuareijet2003@gmail.com>
2024-01-16 10:43:58 +0800

    .

--- CJTechShow/Shared/Domain/Organizers.cs
+++ CJTechShow/Shared/Domain/Organizers.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace CJTechShow.Shared.Domain
 {
-    public class Organizers
+    public class Organizers :BaseDomainModel
     {
         public int OrganizerID { get; set; }
+
+
+        public string? OrganizerName { get; set; }
+        public string? OrganizerContactNumber { get; set; }
+        public string? OrganizerPosition { get; set; }
+        public string? OrganizerEmail { get; set; }
+        public string? OrganizerPassword { get; set; }
+        public virtual Vendors? VendorID {  get; set; }    
     }
+
+    
 }
+
--- CJTechShow/Shared/Domain/Vendors.cs
+++ CJTechShow/Shared/Domain/Vendors.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace CJTechShow.Shared.Domain
 {
-    public class Vendors
+    public class Vendors : BaseDomainModel
     {
         public int VendorID { get; set; }
+        public string? VendorName { get; set; }
+        public string? VendorContactDetails { get; set; }
+        public int BoothNumber { get; set; }
+        public string? VendorDescription { get; set; }
+        public string? Products { get; set; }
+        public string? SocialMedia { get; set; }
+
+        public virtual ITTechShow? EventID { get; set; }
+        public virtual Organizers? OrganizerID { get; set; }
+
+        public virtual Sponsors? Sponsors { get; set;}
     }
 }
