030d3b9b7f3bd76ece61d09b3d5b7dd74dcc0e3d
Notch <chriscreed871@gmail.com>
2024-01-16 10:17:23 +0800

    Created cs. files in Domain Folder for DataBase

---
+++ CJTechShow/Shared/Domain/Organizers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace CJTechShow.Shared.Domain
+{
+    public class Organizer
+    {
+        public int OrganizerID { get; set; }
+
+
+        public string OrganizerName { get; set; }
+        public string OrganizerContactNumber { get; set; }
+        public string OrganizerPosition { get; set; }
+        public string OrganizerEmail { get; set; }
+        public string OrganizerPassword { get; set; }
+     
+        public int VendorID { get; set; }      
+    }
+
+    
+}
+
---
+++ CJTechShow/Shared/Domain/Vendors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJTechShow.Shared.Domain
+{
+    public class Vendors
+    {
+     
+        public int VendorsID { get; set; }
+
+        public string VendorName { get; set; }
+        public string VendorContactDetails { get; set; }
+        public int BoothNumber { get; set; }
+        public string VendorDescription { get; set; }
+        public string Products { get; set; }
+        public string SocialMedia { get; set; }
+
+        public int SponserID { get; set; }
+        public int EventID { get; set; }
+        public int OrganizersID { get; set; }
+        public int VisitorID { get; set; }
+
+    }
+}
